49766cf4c02ce041627e476bef31227760c49ae5
Graner007 <graner.bence007@gmail.com>
2021-07-31 21:27:04 +0200

    add space after card heading

--- src/components/mainpage/ServiceCard.js
+++ src/components/mainpage/ServiceCard.js
@@ -5,7 +5,7 @@ const Service = ({service, url}) => {
     return (
         <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
-                <h5 className="card-title">{service.title}</h5>
+                <h5 className="card-title">{service.title}</h5><br /><br />
                 <Link to={url} className="card-link btn btn-primary">{service.title}</Link>
             </div>
         </div>
