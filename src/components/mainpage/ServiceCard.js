ee6cc2955ccd593649c4f5f5e8c4a1ba00a62056
Graner007 <graner.bence007@gmail.com>
2021-07-31 21:30:06 +0200

    center text in cards

--- src/components/mainpage/ServiceCard.js
+++ src/components/mainpage/ServiceCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Service = ({service, url}) => {
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "18rem", textAlign: "center"}}>
             <div className="card-body">
                 <h5 className="card-title">{service.title}</h5><br /><br />
                 <Link to={url} className="card-link btn btn-primary">{service.title}</Link>
